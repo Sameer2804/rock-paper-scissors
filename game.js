43ee9ebf2b126cc2681a88c1ccd67a6a6a64fef1
Sameer <sameer.ahmed1@kpmg.co.uk>
2023-12-01 17:55:43 +0000

    basic functionality down for the js file

---
+++ game.js
@@ -0,0 +1,47 @@
+const getComputerChoice = () => {
+    const choices = ['Rock', 'Paper', 'Scissors'];
+    return choices[Math.floor(Math.random() * 3)];
+}
+
+const capitilise = string => {
+    return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+}
+
+function promptUser(){
+    let playerSelection = prompt('Rock, Paper or Scissors?');
+    return singleRound(playerSelection, getComputerChoice());
+}
+
+const singleRound = (playerSelection, computerSelection) => {
+
+    playerSelection = capitilise(playerSelection);
+
+    if (playerSelection === 'Rock' && computerSelection === 'Scissors' || playerSelection === 'Scissors' && computerSelection === 'Paper' || playerSelection === 'Paper' && computerSelection === 'Rock'){
+        console.log(`You win! ${playerSelection} beats ${computerSelection}`);
+        return 1;
+    }
+    else if (playerSelection === computerSelection){
+        console.log('It is a tie!');
+        return promptUser(); //Recursion, prompts user again if a tie
+    }
+    else{
+        console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
+        return 0;
+    }
+
+}
+
+function playRound(){
+    let playerScore = 0;
+    let computerScore = 0;
+    for(let i=1; i<=5; i++){
+        promptUser() ? playerScore++ : computerScore++;
+    }
+    (playerScore > computerScore) ? console.log(`You won! With a score of ${playerScore}`) : console.log(`You lose! Computer had score of ${computerScore}`);
+
+    console.log('Player Score: ' + playerScore);
+    console.log('Computer Score: ' + computerScore);
+
+}
+
+playRound();
